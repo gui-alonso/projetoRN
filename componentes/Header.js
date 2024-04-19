ba726d669cbc53ecec6f1334e95c17650c412521
Guilherme Alonso <alonsogui011@gmail.com>
2024-04-19 12:21:24 -0300

    projeto RN

---
+++ componentes/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <Text>Olá do arquivo Header</Text>
+    );  
+}
+
+export default Header;
